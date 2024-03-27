38032be0d9e206ced8849d29e048fc781282f03d
Titima Kasemsritanawat <titima.kp@gmail.com>
2024-03-27 22:11:06 +0700

    Add answer for ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,11 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+let i = 0;
+while(i < restaurants.length){
+    restaurants[i] = "Restaurant Name: " + restaurants[i];
+    newRestaurants.push(restaurants[i]);
+    i++;
+}
 
 console.log(newRestaurants);
